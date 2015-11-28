2311e1d0e8ae82bdf1d1ed9b9ea967757626cfe5
Sebastian Cato <seb.cato@gmail.com>
2015-11-28 15:30:07 +0100

    Read URLs from stdin, README update

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # curl-multi
 
 Learning how to use the libcurl multi API
+
+````
+$ ./curl-multi -h
+Read a list of urls from stdin, print results to stdout
+args:
+  n        maximum number of concurrent transfers (1-100)
+$ time ./curl-multi -n1 < urllist.txt
+http://www.google.com/f (404) 1767B
+http://www.google.com/a (301) 597B
+http://www.google.com/z (404) 1767B
+http://www.google.com/ (302) 484B
+http://lolwut/ (-1) 0B
+http://www.google.se/f (404) 1767B
+http://www.google.se/a (301) 612B
+http://www.google.se/z (404) 1767B
+./curl-multi -n1 < urllist.txt  0,01s user 0,00s system 1% cpu 0,600 total
+$ time ./curl-multi -n8 < urllist.txt
+http://lolwut/ (-1) 0B
+http://www.google.com/ (302) 488B
+http://www.google.se/a (301) 610B
+http://www.google.com/a (301) 597B
+http://www.google.se/f (404) 1767B
+http://www.google.se/z (404) 1767B
+http://www.google.com/z (404) 1767B
+http://www.google.com/f (404) 1767B
+./curl-multi -n8 < urllist.txt  0,00s user 0,01s system 6% cpu 0,218 total
+````
--- main.c
+++ main.c
@@ -1,42 +1,87 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "fetch.h"
 
-struct fetch_data {
-	const char * const *urls;
-	int urlix;
-};
+#define DEFAULT_NCONCURRENT		2
+
+#define IS_TRIM(c) \
+	((c) == ' ' || \
+	(c) == '\r' || \
+	(c) == '\n' || \
+	(c) == '\t')
+
 
 static const char *next_url(void *data) {
-	const char *url;
-	struct fetch_data *urls = data;
+	static char urlbuf[URL_BUFSZ], *cur, *end;
+	for (cur = fgets(urlbuf, URL_BUFSZ, stdin); cur != NULL;
+			cur = fgets(urlbuf, URL_BUFSZ, stdin)) {
+		end = cur+strlen(cur)-1;
+		while(IS_TRIM(*end) && end > cur) {
+			end--;
+		}
 
-	url = urls->urls[urls->urlix];
-	if (url != NULL) {
-		urls->urlix++;
+		*++end='\0';
+
+		while(*cur != '\0' && IS_TRIM(*cur)) {
+			cur++;
+		}
+
+		if (*cur) {
+			break;
+		}
 	}
 
-	return url;
+	return cur;
 }
 
 static void on_complete(struct fetch_transfer *transfer, void *data) {
 	printf("%s (%d) %luB\n", transfer->url, transfer->status, transfer->nrecv);
 }
 
-int main() {
+void usage() {
+	fprintf(stderr,
+		"Read a list of urls from stdin, print results to stdout\n"
+		"args:\n"
+		"  n        maximum number of concurrent transfers (1-100)\n");
+	exit(EXIT_FAILURE);
+}
+
+struct options {
+	int nconcurrent;
+};
+
+void args(int argc, char **argv, struct options *opts) {
+	int ch;
+	char *eptr;
+
+	opts->nconcurrent = DEFAULT_NCONCURRENT;
+	while ((ch = getopt(argc, argv, "hn:")) != -1) {
+		switch(ch) {
+			case 'n':
+				opts->nconcurrent = (int)strtol(optarg, &eptr, 10);
+				if (*eptr != '\0' || opts->nconcurrent < 1 ||
+						opts->nconcurrent > 100) {
+					fprintf(stderr, "invalid n value\n");
+					usage();
+				}
+				break;
+			case '?':
+			case 'h':
+			default:
+				usage();
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
 	fetch_ctx *ctx;
-	const char * const urls[] = {
-		"http://www.example.com/",
-		"http://www.google.com/",
-		"http://www.google.se/",
-		NULL
-	};
+	struct options opts;
 
-	struct fetch_data data;
+	args(argc, argv, &opts);
 
-	data.urls = urls;
-	data.urlix = 0;
-	ctx = fetch_new(2, next_url, on_complete, &data);
+	ctx = fetch_new(opts.nconcurrent, next_url, on_complete, NULL);
 	fetch_event_loop(ctx);
 	fetch_free(ctx);
 	return EXIT_SUCCESS;
