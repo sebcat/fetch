db740ce3e2a0e3dfc696b0fa150df50ef0f72d69
Sebastian Cato <seb.cato@gmail.com>
2015-11-29 10:16:02 +0100

    Add fetch_resolve to the CLI

--- main.c
+++ main.c
@@ -40,24 +40,61 @@ static void on_complete(struct fetch_transfer *transfer, void *data) {
 	printf("%s (%d) %luB\n", transfer->url, transfer->status, transfer->nrecv);
 }
 
+struct options {
+	int nconcurrent;
+	fetch_ctx * ctx;
+};
+
+struct optlist {
+	char *str;
+	struct optlist *next;
+};
+
+static struct optlist *optlist_cons(struct optlist *list, const char *str) {
+	struct optlist *head;
+
+	head = malloc(sizeof(struct optlist));
+	if (head != NULL) {
+		head->str = strdup(str);
+		if (head->str == NULL) {
+			free(head);
+			return NULL;
+		}
+
+		head->next = list;
+	}
+
+	return head;
+}
+
+static void optlist_free(struct optlist *list) {
+	struct optlist *next;
+	if (list != NULL) {
+		for(; list != NULL; list = next) {
+			next = list->next;
+			free(list->str);
+			free(list);
+		}
+	}
+}
+
 static void usage() {
 	fprintf(stderr,
 		"Read a list of urls from stdin, print results to stdout\n"
 		"args:\n"
-		"  n        maximum number of concurrent transfers (1-100)\n");
+		"  n <1-100>           Concurrent transfer limit\n"
+		"  r <host:port:addr>  Resolve host:port to addr\n");
 	exit(EXIT_FAILURE);
 }
 
-struct options {
-	int nconcurrent;
-};
 
 static void args(int argc, char **argv, struct options *opts) {
 	int ch;
 	char *eptr;
+	struct optlist *resolves = NULL, *curr;
 
 	opts->nconcurrent = DEFAULT_NCONCURRENT;
-	while ((ch = getopt(argc, argv, "hn:")) != -1) {
+	while ((ch = getopt(argc, argv, "hn:r:")) != -1) {
 		switch(ch) {
 			case 'n':
 				opts->nconcurrent = (int)strtol(optarg, &eptr, 10);
@@ -67,22 +104,38 @@ static void args(int argc, char **argv, struct options *opts) {
 					usage();
 				}
 				break;
+			case 'r':
+				resolves = optlist_cons(resolves, optarg);
+				if (resolves == NULL) {
+					fprintf(stderr, "optlist_cons malloc error\n");
+					exit(EXIT_FAILURE);
+				}
+				break;
 			case '?':
 			case 'h':
 			default:
 				usage();
 		}
 	}
+
+	opts->ctx = fetch_new(opts->nconcurrent, next_url, on_complete, NULL);
+	if (opts->ctx == NULL) {
+		fprintf(stderr, "fetch_new malloc error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for (curr = resolves; curr != NULL; curr = curr->next) {
+		fetch_resolve(opts->ctx, curr->str);
+	}
+
+	optlist_free(resolves);
 }
 
 int main(int argc, char *argv[]) {
-	fetch_ctx *ctx;
 	struct options opts;
 
 	args(argc, argv, &opts);
-
-	ctx = fetch_new(opts.nconcurrent, next_url, on_complete, NULL);
-	fetch_event_loop(ctx);
-	fetch_free(ctx);
+	fetch_event_loop(opts.ctx);
+	fetch_free(opts.ctx);
 	return EXIT_SUCCESS;
 }
